f44f9a737091630430545a2190e6cdcdb8acea5a
Udit <udit.singh98@gmail.com>
2021-07-01 18:44:49 +0530

    Some more changes

--- Classification/prod_settings.py
+++ Classification/prod_settings.py
@@ -12,7 +12,14 @@ DEBUG = False
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', SECRET_KEY)
 
+INSTALLED_APPS.append('whitenoise.runserver_nostatic')
+MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_USE_FINDERS = True
+
 ALLOWED_HOSTS = ['image-classification-densenet.herokuapp.com']
+
+
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
